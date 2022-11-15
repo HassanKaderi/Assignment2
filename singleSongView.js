93046882564808daad38d6657138246f2e3eb68f
Yuusuf <ymoha464@mtroyal.ca>
2022-11-14 19:59:31 -0800

    Added some stuff

---
+++ singleSongView.js
@@ -0,0 +1,11 @@
+function hideView () {
+    let x = document.getElementById("firstView"); 
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+
+
